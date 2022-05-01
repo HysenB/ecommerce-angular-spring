659239c58375bcc0649f23190f35fa3369ab303e
HysenB <hysi2000@live.com>
2022-05-01 16:28:04 +0200

    maxSize, boundaryLinks and select page size

--- ecommerce-project-release-1.0/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project-release-1.0/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -91,7 +91,14 @@ export class ProductListComponent implements OnInit {
       this.thePageNumber = data.page.number + 1;
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
-    }
+    };
+  }
+
+  updatePageSize(event: Event){
+    const pageSize = (event.target as HTMLInputElement).value;
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 
   
